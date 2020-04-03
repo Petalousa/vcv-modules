2da6beeafef7729c6713e481b3f688c5ba409c35
EDM <moonmanedm@gmail.com>
2020-04-03 17:21:22 -0400

    Add content to readme, fix location of outputs/buttons

--- readme.md
+++ readme.md
@@ -4,3 +4,23 @@
 
 make `.svg` file
 
+run 
+
+
+
+# Notes
+
+You cannot connect VCV and a DAW using a VST bridge: 
+- https://vcvrack.com/manual/FAQ#is-vcv-rack-available-as-a-vst-au-aax-plugin-for-daws
+- https://www.reddit.com/r/vcvrack/comments/deen9b/where_the_hell_is_bridge_vst/
+
+
+
+## Compiling and Installing Plugin
+
+run the following command in MSYS2 MinGW 64-bit!
+
+```
+RACK_DIR="../Rack-SDK" make install
+```
+
--- src/MidiReader.cpp
+++ src/MidiReader.cpp
@@ -3,38 +3,65 @@
 
 struct MidiReader : Module {
 	enum ParamIds {
-		CIRCLE7554_PARAM,
-		CIRCLE7554_2_PARAM,
-		CIRCLE7554_9_PARAM,
+		PLAY_PARAM,
+		STOP_PARAM,
+		LOAD_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
-		CIRCLE7554_2_7_INPUT,
-		CIRCLE7554_2_0_INPUT,
+		PLAY_INPUT,
+		STOP_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
-		CIRCLE7490_OUTPUT,
-		CIRCLE7490_0_OUTPUT,
+		CV_OUTPUT,
+		GATE_OUTPUT,
 		NUM_OUTPUTS
 	};
 	enum LightIds {
 		NUM_LIGHTS
 	};
 
+	dsp::BooleanTrigger playTrigger;
+	dsp::BooleanTrigger stopTrigger;
+	dsp::BooleanTrigger loadTrigger;
+	
+
 	MidiReader() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(CIRCLE7554_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(CIRCLE7554_2_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(CIRCLE7554_9_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(PLAY_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(STOP_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(LOAD_PARAM, 0.f, 1.f, 0.f, "");
 	}
 
+
+
 	void process(const ProcessArgs& args) override {
+		float voltThreshold = 2.5f;
+
+		bool playTapped = params[PLAY_PARAM].getValue() > 0.0f || inputs[PLAY_INPUT].getVoltage() > voltThreshold;
+		bool stopTapped = params[STOP_PARAM].getValue() > 0.0f || inputs[STOP_INPUT].getVoltage() > voltThreshold;
+		bool loadTapped = params[LOAD_PARAM].getValue() > 0.0f;
+
+		if (playTrigger.process(playTapped)){
+			// play button hit
+		}
+		if (stopTrigger.process(stopTapped)){
+			// stop button hit
+		}
+		if (loadTrigger.process(loadTapped)){
+			// load button hit
+		}
+
 	}
+
+	
 };
 
 
 struct MidiReaderWidget : ModuleWidget {
+	TextField* textField;
+
 	MidiReaderWidget(MidiReader* module) {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/MidiReader.svg")));
@@ -44,18 +71,21 @@ struct MidiReaderWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.375, 31.412)), module, MidiReader::CIRCLE7554_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.375, 45.865)), module, MidiReader::CIRCLE7554_2_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(21.375, 60.318)), module, MidiReader::CIRCLE7554_9_PARAM));
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(21.375, 31.412)), module, MidiReader::PLAY_PARAM));
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(21.375, 45.865)), module, MidiReader::STOP_PARAM));
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(21.375, 60.318)), module, MidiReader::LOAD_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(31.712, 31.506)), module, MidiReader::CIRCLE7554_2_7_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(31.712, 46.005)), module, MidiReader::CIRCLE7554_2_0_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(31.712, 31.506)), module, MidiReader::PLAY_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(31.712, 46.005)), module, MidiReader::STOP_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.916, 84.686)), module, MidiReader::CIRCLE7490_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(24.883, 84.686)), module, MidiReader::CIRCLE7490_0_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.916 + 5.0, 84.686 + 20.0)), module, MidiReader::CV_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(24.883 + 5.0, 84.686 + 20.0)), module, MidiReader::GATE_OUTPUT));
+		// + 
 
-		// mm2px(Vec(30.824, 9.657))
-		addChild(createWidget<Widget>(mm2px(Vec(4.498, 12.039))));
+		textField = createWidget<LedDisplayTextField>(mm2px(Vec(4.5, 16.0)));
+		textField->box.size = mm2px(Vec(36.0, 10.657));
+		textField->multiline = true;
+		addChild(textField);
 	}
 };
 
